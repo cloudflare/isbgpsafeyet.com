1a48e67edf822967f230135fd8b2a1b2f12257ef
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-04-10 17:32:41 -0400

    clean up table rendering js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,37 @@
 (() => {
+  const initTable = () => {
+    fetch('data/operators.csv')
+      .then(r => r.text())
+      .then(d3.csvParse)
+      .then(data => {
+        const tbody = document.querySelector('[data-js-table-body]')
+
+        data.forEach(d => {
+          const tr = document.createElement('tr')
+          tr.setAttribute('data-status', d.status.split(' ').join('-'))
+
+          const tdName = document.createElement('td')
+          tdName.innerText = d.name
+          tr.appendChild(tdName)
+
+          const tdType = document.createElement('td')
+          tdType.innerText = d.type
+          tr.appendChild(tdType)
+
+          const tdDetails = document.createElement('td')
+          tdDetails.innerText = d.details
+          tr.appendChild(tdDetails)
+
+          const tdStatus = document.createElement('td')
+          tdStatus.innerText = d.status
+          tdStatus.setAttribute('data-value', `safety-scale-${[, 'safe', 'partially safe', 'unsafe'].indexOf(d.status)}`)
+          tr.appendChild(tdStatus)
+
+          tbody.appendChild(tr)
+        })
+      })
+  }
+
   const initTesting = () => {
     const button = document.querySelector('[data-js-test]')
     const el = document.querySelector('[data-js-test-results]')
@@ -76,6 +109,7 @@
   }
 
   const init = () => {
+    initTable()
     initTesting()
   }
 
