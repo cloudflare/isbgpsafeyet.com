4ce973660f1c184c6010ebde7543abea71a67bc0
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-04-20 19:13:46 -0400

    remove unused function
    this is paired with a commented out example use from the workers sites template this repo started with, which was then removed here:
    
    https://github.com/cloudflare/isbgpsafeyet.com/commit/48fd223b283f2dc36b79b22f6446d736a1765811#diff-a7c5cb691bcea8b2aff31bb0756a2d28L34-L38

--- workers-site/index.js
+++ workers-site/index.js
@@ -187,17 +187,3 @@ class OperatorsTableBuilder {
     })
   }
 }
-
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
-  }
-}
